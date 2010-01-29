d0ad6f26ff8175f736e04c0a71d77da3008364c4
Sunao Komuro <konbu.komuro@gmail.com>
2010-01-29 16:59:52 +0900

    select lua script when startup

--- game.lua
+++ game.lua
@@ -1,18 +1,73 @@
-function execute(file, arguments)
-  io.write("#execute: ")
-  io.write(file)
-  io.write("\n")
-  if arguments then
-    for i,v in ipairs(arguments) do
-      io.write(" ")
-      io.write(v)
+local MAX_Y = 23
+local SCREEN_W = 256
+local SCREEN_H = 192
+local TEXT_H = SCREEN_H / MAX_Y
+
+local profile_start = dslib.profile_start
+local profile_end = dslib.profile_end
+local wait = dslib.wait
+
+local function execute(file, ...)
+  local code,err = loadfile(file)
+  if err then
+    print(err)
+  else
+    io.write("#execute: " .. file)
+    if ... then
+      arg = {...}
+      for i,v in ipairs(arg) do
+        io.write(" ")
+        io.write(v)
+      end
     end
     io.write("\n")
-    arg = arguments
+    dslib.vram_mode()
+    profile_start()
+    code()
+    profile_end()
   end
-  profile_start()
-  dofile(file)
-  profile_end()
 end
-execute("binarytrees.lua", {"10"})
-execute("ao.lua")
+
+local function text_list(list)
+  for i,v in ipairs(list) do
+    dslib.text(0, 0, i-1, v)
+  end
+end
+
+local function select_list(list)
+  dslib.cleartext(0)
+  text_list(list)
+  while true do
+    local function selected(x, y)
+      if x and y then
+        local line = math.floor(y / TEXT_H)
+        if line < #list then
+          return line, list[line+1]
+        end
+      end
+    end
+    local line = selected(dslib.stylus_released())
+    if line then
+      return list[line+1]
+    end
+    line = selected(dslib.stylus_held())
+    if line then
+      text_list(list)
+      dslib.texttilecol(0, 2)
+      dslib.text(0, 0, line, list[line+1])
+      dslib.texttilecol(0, 0)
+    end
+    wait()
+  end
+end
+
+dslib.text_mode()
+
+while true do
+  local files = {dslib.ls()}
+  if files and type(files)=="table" then
+    local selected = select_list(files)
+    execute(selected)
+  end
+  wait()
+end
--- source/main.c
+++ source/main.c
@@ -1,5 +1,10 @@
 #include <PA9.h>
 #include <fat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <setjmp.h>
 
 #include "lua.h"
 #include "lauxlib.h"
@@ -9,7 +14,12 @@
 #define TIMER_DIV TIMER_DIV_256
 #define TIMER_CLOCK (SYSTEM_TIMER_CLOCK/256)
 
-__attribute__((noinline)) static int lua_prfstart(lua_State *L) {
+#define LUA_FUNC static
+
+static jmp_buf mainbuf;
+
+
+__attribute__((noinline)) LUA_FUNC int lua_profilestart(lua_State *L) {
   TIMER0_CR = 0;
   TIMER0_DATA = 0;
   TIMER0_CR = TIMER_ENABLE | TIMER_DIV;
@@ -19,7 +29,7 @@ __attribute__((noinline)) static int lua_prfstart(lua_State *L) {
   return 0;
 }
 
-__attribute__((noinline)) static int lua_prfend(lua_State *L) {
+__attribute__((noinline)) LUA_FUNC int lua_profileend(lua_State *L) {
   vu32 t0d = TIMER0_DATA;
   vu32 t1d = TIMER1_DATA;
   u32 clk = (t1d<<16) | t0d;
@@ -31,7 +41,7 @@ inline static void draw(int x, int y, int r, int g, int b) {
   PA_Put16bitPixel(0, x, y, PA_RGB(r, g, b));
 }
 
-__attribute__((noinline)) static int lua_draw(lua_State *L) {
+LUA_FUNC int lua_draw(lua_State *L) {
   int x = luaL_checkint(L, -5);
   int y = luaL_checkint(L, -4);
   int r = luaL_checkint(L, -3);
@@ -43,37 +53,126 @@ __attribute__((noinline)) static int lua_draw(lua_State *L) {
   return 0;
 }
 
+LUA_FUNC int lua_waitvbl(lua_State *L) {
+  PA_WaitForVBL();
+  return 0;
+}
+
+LUA_FUNC int lua_vram_mode(lua_State *L) {
+  PA_SetBgPalCol(0, 0, PA_RGB(0, 0, 0));
+  PA_Init16bitBg(0, 3);
+  return 0;
+}
+
+LUA_FUNC int lua_text_mode(lua_State *L) {
+  PA_InitText(0, 0);
+  PA_SetTextCol(0, 31, 31, 31);
+  return 0;
+}
+
+LUA_FUNC int lua_textcol(lua_State *L) {
+  int screen = luaL_checkint(L, -4);
+  int r = luaL_checkint(L, -3);
+  int g = luaL_checkint(L, -2);
+  int b = luaL_checkint(L, -1);
+  PA_SetTextCol(screen, r, g, b);
+  return 0;
+}
+
+LUA_FUNC int lua_texttilecol(lua_State *L) {
+  int screen = luaL_checkint(L, -2);
+  int n = luaL_checkint(L, -1);
+  PA_SetTextTileCol(screen, n);
+  return 0;
+}
+
+LUA_FUNC int lua_text(lua_State *L) {
+  int screen = luaL_checkint(L, -4);
+  int x = luaL_checkint(L, -3);
+  int y = luaL_checkint(L, -2);
+  const char *text = luaL_checklstring(L, -1, NULL);
+  u16 letters = PA_OutputSimpleText(screen, x, y, text);
+  lua_pushinteger(L, letters);
+  return 1;
+}
+
+LUA_FUNC int lua_cleartext(lua_State *L) {
+  s8 screen = luaL_checkint(L, -1);
+  PA_ClearTextBg(screen);
+  return 0;
+}
+
+LUA_FUNC int lua_stylus(lua_State *L) {
+  lua_pushinteger(L, Stylus.X);
+  lua_pushinteger(L, Stylus.Y);
+  return 2;
+}
+
+LUA_FUNC int lua_stylus_held(lua_State *L) {
+  if (!Stylus.Held) return 0;
+  return lua_stylus(L);
+}
+
+LUA_FUNC int lua_stylus_released(lua_State *L) {
+  if (!Stylus.Released) return 0;
+  return lua_stylus(L);
+}
+
+LUA_FUNC int lua_stylus_newpress(lua_State *L) {
+  if (!Stylus.Newpress) return 0;
+  return lua_stylus(L);
+}
+
+LUA_FUNC int lua_ls(lua_State *L) {
+  DIR *cdir;
+  struct dirent *ent;
+  struct stat statbuf;
+  s16 filecount = 0;
+
+  lua_text_mode(L);
+
+  cdir = opendir(".");
+  if (!cdir) {
+    iprintf("cannot open ./\n");
+    exit(1);
+  }
+
+  while ((ent = readdir(cdir)) != NULL) {
+    char *dname = ent->d_name;
+    stat(dname, &statbuf);
+    if (S_ISDIR(statbuf.st_mode)) continue;
+    if (!strstr(dname, ".lua")) continue;
+    lua_pushstring(L, dname);
+    ++filecount;
+  }
+  closedir(cdir);
+
+  return filecount;
+}
+
+static const luaL_Reg ds_funcs[] = {
+  {"profile_start", lua_profilestart},
+  {"profile_end", lua_profileend},
+  {"draw", lua_draw},
+  {"wait", lua_waitvbl},
+  {"ls", lua_ls},
+  {"vram_mode", lua_vram_mode},
+  {"text_mode", lua_text_mode},
+  {"text", lua_text},
+  {"textcol", lua_textcol},
+  {"texttilecol", lua_texttilecol},
+  {"stylus", lua_stylus},
+  {"stylus_held", lua_stylus_held},
+  {"stylus_released", lua_stylus_released},
+  {"stylus_newpress", lua_stylus_newpress},
+  {"cleartext", lua_cleartext},
+  {NULL, NULL}
+};
+
 typedef struct {
   char *luapath;
   lua_State *L;
 } GameState;
-
-static void init_lua(GameState *state) {
-  state->L = lua_open();
-  luaL_openlibs(state->L);
-  lua_register(state->L, "profile_start", lua_prfstart);
-  lua_register(state->L, "profile_end", lua_prfend);
-  lua_register(state->L, "draw", lua_draw);
-}
-
-GameState *init_state() {
-  static GameState state;
-  PA_Init();
-  consoleDemoInit();
-
-  PA_SetBgPalCol(0, 0, PA_RGB(0, 0, 0));
-  PA_Init16bitBg(0, 3);
-  iprintf("Nintendo DS Game Demo!\n");
-
-  if (!fatInitDefault()) {
-    iprintf("cannot init libfat\n");
-    exit(1);
-  }
-
-  init_lua(&state);
-
-  return &state;
-}
 
 static void loadlua(GameState *state, const char *luapath) {
   iprintf("load: %s\n", luapath);
@@ -92,30 +191,57 @@ static void testlua(GameState *state, const char *luapath) {
   runlua(state);
 }
 
+static void init_lua(GameState *state) {
+  if (state->L) {
+    lua_close(state->L);
+  }
+  state->L = lua_open();
+  luaL_openlibs(state->L);
+  luaL_register(state->L, "dslib", ds_funcs);
+}
+
+static void pad_vbl() {
+  if (Pad.Released.Start) {
+    longjmp(mainbuf, 1);
+  }
+}
+
+GameState *init_state() {
+  static GameState state = {NULL, NULL};
+
+  PA_Init();
+  consoleDemoInit();
+
+  iprintf("Nintendo DS Game Demo!\n");
+
+  if (!fatInitDefault()) {
+    iprintf("cannot init libfat\n");
+    exit(1);
+  }
+
+  GHPadVBL = pad_vbl;
+
+  init_lua(&state);
+
+  return &state;
+}
+
 void mainloop(GameState *state) {
   testlua(state, "game.lua");
 
-  //s16 b = 0;
-  //s16 c = 0;
-  //while(1) {
-  //  s16 i;
-  //  iprintf("c(%d,%d,%d)\n", c, c, c);
-  //  for (i=b;i<b+128;++i) {
-  //    s16 j;
-  //    for (j=b;j<b+128;++j) {
-  //      draw(i, j, c, c, c);
-  //    }
-  //  }
-  //  //PA_WaitForVBL();
-  //  if (++b >= SCREEN_HEIGHT) b = 0;
-  //  if (++c >= 32) c=0;
-  //}
+  while(1) {
+    PA_WaitForVBL();
+  }
 }
 
 int main(int argc, char **argv) {
-  GameState *state = init_state();;
+  while (1) {
+    if (setjmp(mainbuf) == 0) {
+      GameState *state = init_state();
 
-  mainloop(state);
+      mainloop(state);
+    }
+  }
 
   return 0;
 }
